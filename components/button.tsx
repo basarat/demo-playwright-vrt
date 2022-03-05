9cb6201c9afcba15049a1b006f916e8ab0effc3d
basarat <basaratali@gmail.com>
2022-03-06 09:54:43 +1100

    changes

--- components/button.tsx
+++ components/button.tsx
@@ -2,6 +2,6 @@ import styles from './button.module.css';
 
 export const Button = () => {
   return (<button className={styles.button}>
-    Playwright is AWESOME
+    Playwright is AWESOME 🌹
   </button>);
 }
